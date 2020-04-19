7d6b569c488a6e8a5e45ead71ed20a86ba06ef98
SL <38764885+newdisc@users.noreply.github.com>
2020-04-19 13:10:47 +0530

    JobStatus toString

--- src/main/java/nd/sched/job/IJobExecutor.java
+++ src/main/java/nd/sched/job/IJobExecutor.java
@@ -5,6 +5,10 @@ public interface IJobExecutor {
     public static class JobReturn {
         public String returnValue;
         public JobStatus jobStatus;
+        @Override
+        public String toString(){
+            return returnValue + " - " + jobStatus;
+        }
     };
     public String getName();
     public JobReturn execute(String argumentString);
